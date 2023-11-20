d06b152e56317d4f802044e3445f63fe6887d2dd
Emmett Watkins-Brown <emmettwb@gmail.com>
2023-11-20 02:06:41 -0700

    autopep8

--- app_flask/__init__.py
+++ app_flask/__init__.py
@@ -2,19 +2,19 @@ from os import environ
 
 from flask import Flask
 
+from . import routes
+from .composer import composer
+from .config import CONFIG
+
 app = Flask(__name__)
 
-# Read in the configuration file
-from .config import CONFIG
 
 app.secret_key = 'insecure key please change'
 app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
 app.config['MAX_CONTENT_LENGTH'] = CONFIG.max_upload_size_mb * 1024 * 1024
 
-from . import routes
 
 # Set up API endpoints
 # from .api import api
 # app.register_blueprint(api, url_prefix=r'/api')
-from .composer import composer
 app.register_blueprint(composer, url_prefix=r'/compose')
