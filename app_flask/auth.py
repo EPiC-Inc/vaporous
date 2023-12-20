4511a51291d0d060eff654fce81685cd6aaf662f
Emmett Watkins-Brown <emmettwb@gmail.com>
2023-12-19 20:17:58 -0700

    password checking fix

--- app_flask/auth.py
+++ app_flask/auth.py
@@ -17,6 +17,7 @@ class Session:
     home: str
     expires: datetime
 
+
 valid_username_regex = compile(r"^\w+$")
 
 SCRYPT_SETTINGS = {"n": 2**12, "r": 8, "p": 1}
@@ -39,20 +40,46 @@ def login(username: str, password: str | bytes):
         return False
     stored_username, stored_hash, user_level = result[0]
     password_hash = scrypt(password, salt=username.encode(), **SCRYPT_SETTINGS)
-    print(username, stored_username, password_hash, stored_hash)
+    if not str(password_hash) == str(stored_hash):
+        return False
     # Prevents duplicate uuids just in case
     while SESSIONS.get(id := str(uuid4())):
         pass
     SESSIONS[id] = Session(
         username=username,
         user_level=user_level,
-        home=f"home/{username}" if user_level > 0 else '.',
+        home=f"home/{username}" if user_level > 0 else ".",
         expires=datetime.now() + SESSION_EXPIRY,
     )
     return id
 
 
-def add_user(username: str, password: str | bytes, *, user_level: int = 99) -> tuple[bool, str]:
+def update_password(
+    username: str, old_password: str | bytes, new_password: str | bytes
+) -> tuple[bool, str]:
+    result = user_table.query(
+        "username, password", where_column="username", where_data=[username]
+    )
+    if not result:
+        return False, "Somehow, you aren't a user!"
+    stored_username, stored_hash = result[0]
+    if isinstance(old_password, str):
+        old_password = old_password.encode()
+    if isinstance(new_password, str):
+        new_password = new_password.encode()
+    old_password_hash = scrypt(old_password, salt=username.encode(), **SCRYPT_SETTINGS)
+    if not str(old_password_hash) == str(stored_hash):
+        return False, "Invalid password"
+    new_password_hash = scrypt(new_password, salt=username.encode(), **SCRYPT_SETTINGS)
+    user_table.update_property(
+        "password", str(new_password_hash), where_column="username", where_data=username
+    )
+    return True, "Success"
+
+
+def add_user(
+    username: str, password: str | bytes, *, user_level: int = 99
+) -> tuple[bool, str]:
     if not (username and password):
         return False, "Blank username and password"
     if len(username) > 40:
