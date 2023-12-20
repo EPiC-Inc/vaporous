37109ea41f82d18d5ea546abaf4f6a19a2992c1c
Emmett Watkins-Brown <emmettwb@gmail.com>
2023-12-19 19:29:11 -0700

    css tweak

--- add_user.py
+++ add_user.py
@@ -4,5 +4,6 @@ from app_flask import auth
 
 NEW_USER = input("new username: ")
 NEW_PASS = input("password: ")
-success, msg = auth.add_user(NEW_USER, NEW_PASS)
+NEW_LEVEL = input("user level (0 is admin, 99 default): ") or 99
+success, msg = auth.add_user(NEW_USER, NEW_PASS, user_level=int(NEW_LEVEL))
 assert success, f"User add failed: {msg}"
