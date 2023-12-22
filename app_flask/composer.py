a4f5d26a905468ed87810ff07133ee8c189005f0
Emmett Watkins-Brown <emmettwb@gmail.com>
2023-12-22 02:03:35 -0700

    New folders can be created

--- app_flask/composer.py
+++ app_flask/composer.py
@@ -2,8 +2,8 @@ from flask import Blueprint, redirect, render_template, request, session, url_fo
 from werkzeug.wrappers.response import Response
 
 from . import CONFIG
-from .file_api import list_files, retrieve, save_file
 from .auth import get_session
+from .file_api import list_files, new_folder, retrieve, save_file
 
 composer = Blueprint("composer", __name__)
 
@@ -12,13 +12,13 @@ composer = Blueprint("composer", __name__)
 @composer.route("/dir_view/<path:base_directory>")
 def compose_file_list(base_directory="") -> str | Response:
     # TODO - check if user has access
-    user = get_session(session.get("id", ''))
+    user = get_session(session.get("id", ""))
     if not user:
-        return redirect(url_for('login_page'))
-    home = user.home if user.user_level > 0 else '.'
+        return redirect(url_for("login_page"))
+    home = user.home if user.user_level > 0 else "."
     viewable_files = list_files(f"{home}/{base_directory}")
     for file_ in viewable_files.values():
-        file_.path = file_.path[len(user.home):] if user.user_level > 0 else file_.path
+        file_.path = file_.path[len(user.home) :] if user.user_level > 0 else file_.path
     paths = []
     current_path = ""
     for path in base_directory.split("/"):
@@ -37,7 +37,7 @@ def compose_file_list(base_directory="") -> str | Response:
 
 @composer.post("/upload")
 def upload_file() -> tuple[str, int]:
-    user = get_session(session.get("id", ''))
+    user = get_session(session.get("id", ""))
     if not user:
         return "Not authenticated", 401
     files = request.files.getlist("file")
@@ -51,19 +51,41 @@ def upload_file() -> tuple[str, int]:
         save_file(f"{user.home}/{upload_path}", file)
     return "Success", 200
 
+
+@composer.route("/new_folder", methods=["POST"])
+def add_new_folder():
+    user = get_session(session.get("id", ""))
+    if not user:
+        return "Not authenticated", 401
+    data = request.json
+    if not data:
+        return "Data in invalid format, must be JSON", 400
+    current_path = data.get("current_path")
+    folder_name = data.get("folder_name")
+    if not folder_name:
+        return [False, "Folder name cannot be blank"]
+    current_path = f"{user.home}/{current_path}"
+    success = new_folder(current_path, folder_name)
+    if success:
+        return [True, "Success"]
+    return [False, "Unable to create folder"]
+
+
 @composer.route("/rename", methods=["POST"])
 @composer.route("/rename/<path:to_rename>", methods=["POST"])
 def rename_file(to_rename: str = "") -> tuple[str, int]:
-    if not get_session(session.get("id", '')):
+    if not get_session(session.get("id", "")):
         return "Not authenticated", 401
-    return '', 501
+    return "", 501
+
 
 @composer.route("/delete", methods=["GET", "POST"])
 @composer.route("/delete/<path:to_delete>", methods=["GET", "POST"])
 def delete_file(to_delete: str = "") -> tuple[str, int]:
-    if not get_session(session.get("id", '')):
+    if not get_session(session.get("id", "")):
         return "Not authenticated", 401
-    return '', 501
+    return "", 501
+
 
 @composer.errorhandler(FileNotFoundError)
 def handle_FileNotFoundError(e) -> str:
