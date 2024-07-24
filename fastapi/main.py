ad00fa26ef4626be3d239b4f1e5c85754f52ce28
labs <emmettwb@gmail.com>
2024-07-24 09:18:48 -0600

    ignore the duplicated files

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates("templates")
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("login.html", {"request": Request, "name": "test"})
+    return templates.TemplateResponse(request=request, name="login.html")
+
+@app.post("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse(request=request, name="login.html", context={"messages": [("error", "Not implemented yet :V")]})
