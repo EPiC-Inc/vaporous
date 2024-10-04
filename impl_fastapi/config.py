a159903a9b998b338773a11a61b23823daf35689
Emmett Watkins-Brown <emmettwb@gmail.com>
2024-10-03 18:04:04 -0600

    Redundant

--- impl_fastapi/config.py
+++ impl_fastapi/config.py
@@ -1,4 +1,4 @@
-"""Config module to ensure config can be accessed from multiple modules."""
+"""Module to ensure config can be accessed from multiple modules."""
 
 from pathlib import Path
 from tomllib import load as toml_load
