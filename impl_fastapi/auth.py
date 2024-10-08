10846d9186bfb37f4500d2c0dd93e03a58580790
Emmett Watkins-Brown <emmettwb@gmail.com>
2024-10-08 00:20:34 -0600

    Testing, more auth, better column names

--- impl_fastapi/auth.py
+++ impl_fastapi/auth.py
@@ -11,9 +11,10 @@ from objects import User
 INVALID_USERNAME_CHARACTERS = regex_compile(r'<|>|:|"|\?|\/|\\|\||\*')
 USERNAME_LENGTH: int = 24
 
-COST_PARAMETER: int = 2**3
+COST_PARAMETER: int = 2**12
 BLOCK_SIZE: int = 8
 PARALLELIZATION: int = 4
+HASH_LENGTH: int = 32
 
 def passkey_challenge() -> bytes:
     return token_bytes(14)
@@ -23,6 +24,26 @@ def validate_username(username: str) -> bool:
     if len(username) > USERNAME_LENGTH:
         return False
     return not bool(INVALID_USERNAME_CHARACTERS.search(username))
+
+
+def checkpw(password: str, stored_hash: str) -> bool:
+    (
+        stored_salt,
+        stored_cost_parameter,
+        stored_block_size,
+        stored_parallelization,
+        stored_hash,
+    ) = stored_hash.split('$', 4)
+
+    computed_hash = scrypt(
+        password.encode(),
+        salt=bytes.fromhex(stored_salt),
+        n=int(stored_cost_parameter),
+        r=int(stored_block_size),
+        p=int(stored_parallelization),
+        dklen=len(stored_hash) // 2
+    )
+    return stored_hash == computed_hash.hex()
 
 
 def add_user(username: str, *, password: Optional[str] = None, passkey_token=None) -> tuple[bool, str | set[str]]:
@@ -36,20 +57,23 @@ def add_user(username: str, *, password: Optional[str] = None, passkey_token=Non
     results: set[str] = set()
     if password:
         password = password[:512]
+        salt = token_bytes(16)
         password_hash = scrypt(
             password.encode(),
-            salt=username.encode(),
+            salt=salt,
             n=COST_PARAMETER,
             r=BLOCK_SIZE,
             p=PARALLELIZATION,
-            dklen=64,
+            dklen=HASH_LENGTH,
         )
-        print(password_hash)
+        stored_hash = f"{salt.hex()}${COST_PARAMETER}${BLOCK_SIZE}${PARALLELIZATION}${password_hash.hex()}"
+        print(stored_hash) #TEMP
+        print(checkpw(password, stored_hash)) #TEMP
         results.add("password")
     if passkey_token:
         results.add("passkey")
+    # TODO - create User object and add it to database
     return (True, results)
-    # print(User(username=username, password=password_hash))
 
-
-# print(add_user("test", password="test2")) # TEMP
+if __name__ == '__main__':
+    print(add_user("test", password="test2")) # TEMP
--- impl_fastapi/tests/integration/test_server_active.py
+++ impl_fastapi/tests/integration/test_server_active.py
@@ -6,9 +6,13 @@ from fastapi.responses import Response
 from fastapi.templating import Jinja2Templates
 from fastapi.testclient import TestClient
 
-from impl_fastapi.main import app
+try:
+    from impl_fastapi.main import app
+except:
+    from main import app
 
-ACCESSIBLE_WHEN_LOGGED_OUT = ("/", "/login")
+ACCESSIBLE_WHEN_LOGGED_OUT: tuple[list[str, str]] = (["/", "/login"], ["/login", "/login"])
+PREPEND_URL = "http://testserver"
 
 class TestServer(TestCase):
     def setUp(self):
@@ -18,10 +22,11 @@ class TestServer(TestCase):
         self.assertIsNotNone(self.test_app.app_state)
 
     def test_server_pages_logged_out(self):
-        for page in ACCESSIBLE_WHEN_LOGGED_OUT:
+        for page, redirect_to in ACCESSIBLE_WHEN_LOGGED_OUT:
             with self.subTest(msg=f"Page: {page}"):
                 response = self.test_app.get(page)
                 self.assertEqual(response.status_code, 200)
+                self.assertEqual(str(response.url), f"{PREPEND_URL}{redirect_to}")
 
     def tearDown(self):
         self.test_app.close()
