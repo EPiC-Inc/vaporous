e95034160918a2daf944e85f1353c94aa4e252dd
Emmett Watkins-Brown <emmettwb@gmail.com>
2024-10-16 00:20:38 -0600

    Now automatically creates config.toml from the defaults

--- impl_fastapi/config.py
+++ impl_fastapi/config.py
@@ -5,5 +5,10 @@ from tomllib import load as toml_load
 
 CONFIG: dict
 
-with open(Path(__file__).parent / "config.toml", "rb") as config_file:
+config_path = Path(__file__).parent / "config.toml"
+
+if not config_path.exists():
+    config_path.write_text((Path(__file__).parent / "config_default.toml").read_text())
+
+with open(config_path, "rb") as config_file:
     CONFIG = toml_load(config_file)
